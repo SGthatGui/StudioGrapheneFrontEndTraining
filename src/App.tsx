074e7c494cc2766a40ab9dc187831e2f0ec2871c
thatGui <guilherme.fonseca@studiographene.com>
2022-08-08 14:20:12 +0100

    further cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 //section for util imports
 import React from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 //section for component imports
 
 //section for React.FC
 const App: React.FC = () => {
+  //
+
   return (
-    <div>
+    <div className='base'>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        <h1>StudioGraphene Training program</h1>
       </div>
-      <h1>Vite + React</h1>
     </div>
   )
 }
